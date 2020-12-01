358bfa6c60ea77e246964ef560f578a0a03cc044
Nguyễn Đức Trung <nguyenductrung6893@hotmail.com>
2020-12-01 23:21:23 +0700

    create solution structure

---
+++ StudyWithNgocWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudyWithNgocWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
